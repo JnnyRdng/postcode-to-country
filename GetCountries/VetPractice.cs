66122bd34c3ece87580b26142ff221362d7a49ac
Jonny Reading <jonnyreading@gmail.com>
2025-01-28 22:43:23 +0000

    fix up files

--- GetCountries/VetPractice.cs
+++ GetCountries/VetPractice.cs
@@ -9,27 +9,4 @@ public class VetPractice
     
     [Name("country")]
     public string Country { get; set; } = string.Empty;
-    
-    // [Name("org_id")]
-    // public long OrgId { get; set; }
-    //
-    // [Name("contact_email")]
-    // public string ContactEmail { get; set; } = string.Empty;
-    //
-    // [Name("contact_name")]
-    // public string ContactName { get; set; } = string.Empty;
-    //
-    // [Name("name")]
-    // public string Name { get; set; } = string.Empty;
-    //
-    // [Name("county")]
-    // public string County { get; set; } = string.Empty;
-    //
-    // [Name("area")]
-    // public string Area { get; set; } = string.Empty;
-    //
-    // [Name("town")]
-    // public string Town { get; set; } = string.Empty;
-    //
-    
 }
